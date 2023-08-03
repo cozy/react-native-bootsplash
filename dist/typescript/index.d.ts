050aaa764c536f82bc83e087c136695f7760f041
Ldoppea <yannick.chiron@gmail.com>
2023-08-03 12:57:23 +0200

    feat: Do not gitignore `/dist` to allow using this Github as NPM source
    To use this lib in our project we will `yarn prepare` and commit
    `/dist` content
    
    This will allow us to add this lib in the `package.json` by using the
    Github URL
    
    We cannot use the `prepare` entry from `package.json` to install it
    from the consumer project due to a bug when using Github as source with
    yarn 1.x (cf yarnpkg/yarn#5235)

---
+++ dist/typescript/index.d.ts
@@ -0,0 +1,14 @@
+export declare type VisibilityStatus = "visible" | "hidden" | "transitioning";
+export declare type Config = {
+    fade?: boolean;
+    bootsplashName?: string;
+};
+export declare function show(config?: Config): Promise<void>;
+export declare function hide(config?: Config): Promise<void>;
+export declare function getVisibilityStatus(): Promise<VisibilityStatus>;
+declare const _default: {
+    show: typeof show;
+    hide: typeof hide;
+    getVisibilityStatus: typeof getVisibilityStatus;
+};
+export default _default;
